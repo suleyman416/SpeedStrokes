37e2df66ac6dbdea921ac9c2e338030d4a0e25d5
Suleyman Mammadov <suleyman@Suleymans-MacBook-Air-2.local>
2025-08-21 13:17:53 +0400

    Force cache refresh: Add aggressive cache-busting headers for Netlify CDN

--- pages/_app.js
+++ pages/_app.js
@@ -52,6 +52,9 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <title>SpeedStrokes - Typing Practice App</title>
+        <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+        <meta http-equiv="Pragma" content="no-cache" />
+        <meta http-equiv="Expires" content="0" />
         <meta name="description" content="A beautiful typing practice app to improve your typing speed and accuracy" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#3b82f6" />
